5e75cc59be272f2217a8f1e23fdf1a2fabcae19b
alejandrobraun <ufx559@my.utsa.edu>
2016-10-04 20:52:25 -0500

    Update cashflow.jl

--- cashflow.jl
+++ cashflow.jl
@@ -1,9 +1,4 @@
 tvmnpv(n,i,cf0,cfall)=-(cf0+sum(cfall./(1+i).^n))
 tvmpmt(n,i,pv)=-(i*pv)/(1-(1+i)^-n)
 tvmpv(n,i,pmt)=-sum(pmt.*(1+i).^(-n))
-## tvmpv can also be sum(pmt./(1+r).^(p))
 tvmfv(n,i,pmt)=-sum(pmt./(1+i).^(-n+1))
-## for tvmfv, there is -n+1 because the last cashflow happens at the time of the fv
-## tvmfv can also be sum(pmt.*(1+i).^(n-1))
-
-
