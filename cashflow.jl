4e4a4803046a3cd76e2c6e606a2b0bce3318a3e1
alejandrobraun <ufx559@my.utsa.edu>
2016-10-05 17:47:33 -0500

    Update cashflow.jl

--- cashflow.jl
+++ cashflow.jl
@@ -1,4 +1,16 @@
+module Cashflows
+
+export tvmnpv, tvmpmt, tvmpv, tvmfv, cfnoi
+
+# Basic TVM Functions
 tvmnpv(n,i,cf0,cfall)=-(cf0+sum(cfall./(1+i).^n))
 tvmpmt(n,i,pv)=-(i*pv)/(1-(1+i)^-n)
 tvmpv(n,i,pmt)=-sum(pmt.*(1+i).^(-n))
 tvmfv(n,i,pmt)=-sum(pmt./(1+i).^(-n+1))
+
+# Operating Statement
+cfnoi(pgi,vcr,oexr,capexr)=pgi-pgi.*vcr-(pgi-pgi.*vcr)*oexr-(pgi-pgi.*vcr)*capexr
+
+importall Cashflows
+
+end # module
