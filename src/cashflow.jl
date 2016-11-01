59c56e97fdcc3823ed944e6acf7e95cf9552bd9e
alejandrobraun <ufx559@my.utsa.edu>
2016-11-01 16:48:38 -0500

    Update cashflow.jl

--- src/cashflow.jl
+++ src/cashflow.jl
@@ -3,10 +3,17 @@ module Cashflows
 export tvmnpv, tvmpmt, tvmpv, tvmfv, cfnoi
 
 # Basic TVM Functions
-tvmnpv(n,i,cf0,cfall)=-(cf0+sum(cfall./(1+i).^n))
+tvmnpv(i,cfo,cfall)=begin
+	n=collect(1:length(cfall));
+	cfo + sum(cfall./(1+i).^n)
+	end
 tvmpmt(n,i,pv)=-(i*pv)/(1-(1+i)^-n)
 tvmpv(n,i,pmt)=-sum(pmt.*(1+i).^(-n))
-tvmfv(n,i,pmt)=-sum(pmt./(1+i).^(-n+1))
+tvmfv(n,i,pv,pmt)= begin
+	n=collect(1:n)
+	p=length(n)
+	pv*(1+i)^p-sum(-pmt./(1+i).^(-n+1))
+	end
 
 # Operating Statement
 cfnoi(pgi,vcr,oexr,capexr)=pgi-pgi.*vcr-(pgi-pgi.*vcr)*oexr-(pgi-pgi.*vcr)*capexr
